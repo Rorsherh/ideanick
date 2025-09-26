fa4e84f8c843bf20792a7b4f6decbe15c150ef8b
kuzmin_ae <aekuz89@gmail.com>
2025-09-26 23:44:51 +0300

    мелкий косметический перенос описания на новую строку

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,29 +3,34 @@ export const App = () => {
     {
       nick: "cool-idea-nick-1",
       name: "Idea 1",
-      description: "Lorem 1 ipsum dolor sit amet consectetur adipisicing elit...",
+      description:
+        "Lorem 1 ipsum dolor sit amet consectetur adipisicing elit...",
     },
     {
       nick: "cool-idea-nick-2",
       name: "Idea 2",
-      description: "Lorem 2 ipsum dolor sit amet consectetur adipisicing elit...",
+      description:
+        "Lorem 2 ipsum dolor sit amet consectetur adipisicing elit...",
     },
     {
       nick: "cool-idea-nick-3",
       name: "Idea 3",
-      description: "Lorem 3 ipsum dolor sit amet consectetur adipisicing elit...",
+      description:
+        "Lorem 3 ipsum dolor sit amet consectetur adipisicing elit...",
     },
     {
       nick: "cool-idea-nick-4",
       name: "Idea 4",
-      description: "Lorem 4 ipsum dolor sit amet consectetur adipisicing elit...",
+      description:
+        "Lorem 4 ipsum dolor sit amet consectetur adipisicing elit...",
     },
     {
       nick: "cool-idea-nick-5",
       name: "Idea 5",
-      description: "Lorem 5 ipsum dolor sit amet consectetur adipisicing elit...",
+      description:
+        "Lorem 5 ipsum dolor sit amet consectetur adipisicing elit...",
     },
-  ]
+  ];
   return (
     <div>
       <h1>Ideanick</h1>
@@ -35,8 +40,8 @@ export const App = () => {
             <h2>{idea.name}</h2>
             <p>{idea.description}</p>
           </div>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
