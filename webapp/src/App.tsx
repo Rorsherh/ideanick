be5a0ce19d58ed047930dfcbf6b20594f28b5141
kuzmin_ae <aekuz89@gmail.com>
2025-09-26 21:26:15 +0300

    создал самый первый набросок страницы список идей

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
+    <div>
+      <h1>Ideanick</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite408 + React23nov</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <div>
+          <h2>Idea 1</h2>
+          <p>Lorem 1 ipsum dolor sit amet consectetur adipisicing elit...</p>
+        </div>
 
-export default App
+        <div>
+          <h2>Idea 2</h2>
+          <p>Lorem 2 ipsum dolor sit amet consectetur adipisicing elit...</p>
+        </div>
+
+        <div>
+          <h2>Idea 3</h2>
+          <p>Lorem 2 ipsum dolor sit amet consectetur adipisicing elit...</p>
+        </div>
+      </div>
+    </div>
+  ); 
+}
