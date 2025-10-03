7b2cb363ef835944169c033f3c420630af45dd33
kuzmin_ae <aekuz89@gmail.com>
2025-10-03 16:50:48 +0300

    Создание небольшого express приложения

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello Alex!")
+import express from 'express';
+
+const expressApp = express();
+expressApp.get('/ping', (reg, res) => {
+    res.send('pong');
+})
+expressApp.listen(3000, () =>{
+    console.info('Listening at http://localhost:3000');
+})
