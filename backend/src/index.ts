bc72abcd7ad6979edba8c364970517ea29b45450
kuzmin_ae <aekuz89@gmail.com>
2025-10-03 17:48:04 +0300

    Добавление tRPC в бэкенд. Это нужно для синхронизации типов

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,46 +1,15 @@
 import express from 'express';
-
-const ideas = [
-    {
-      nick: "cool-idea-nick-1",
-      name: "Idea 1",
-      description:
-        "Lorem 1 ipsum dolor sit amet consectetur adipisicing elit...",
-    },
-    {
-      nick: "cool-idea-nick-2",
-      name: "Idea 2",
-      description:
-        "Lorem 2 ipsum dolor sit amet consectetur adipisicing elit...",
-    },
-    {
-      nick: "cool-idea-nick-3",
-      name: "Idea 3",
-      description:
-        "Lorem 3 ipsum dolor sit amet consectetur adipisicing elit...",
-    },
-    {
-      nick: "cool-idea-nick-4",
-      name: "Idea 4",
-      description:
-        "Lorem 4 ipsum dolor sit amet consectetur adipisicing elit...",
-    },
-    {
-      nick: "cool-idea-nick-5",
-      name: "Idea 5",
-      description:
-        "Lorem 5 ipsum dolor sit amet consectetur adipisicing elit...",
-    },
-  ];
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { trpcRouter } from './trpc';
 
 const expressApp = express();
 expressApp.get('/ping', (reg, res) => {
     res.send('pong');
 })
 
-expressApp.get('/ideas', (req, res) => {
-    res.send(ideas)
-})
+expressApp.use('/trpc', trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+}))
 
 expressApp.listen(3000, () =>{
     console.info('Listening at http://localhost:3000');
---
+++ backend/src/trpc.ts
@@ -0,0 +1,44 @@
+import {initTRPC} from '@trpc/server';
+
+const ideas = [
+    {
+      nick: "cool-idea-nick-1",
+      name: "Idea 1",
+      description:
+        "Lorem 1 ipsum dolor sit amet consectetur adipisicing elit...",
+    },
+    {
+      nick: "cool-idea-nick-2",
+      name: "Idea 2",
+      description:
+        "Lorem 2 ipsum dolor sit amet consectetur adipisicing elit...",
+    },
+    {
+      nick: "cool-idea-nick-3",
+      name: "Idea 3",
+      description:
+        "Lorem 3 ipsum dolor sit amet consectetur adipisicing elit...",
+    },
+    {
+      nick: "cool-idea-nick-4",
+      name: "Idea 4",
+      description:
+        "Lorem 4 ipsum dolor sit amet consectetur adipisicing elit...",
+    },
+    {
+      nick: "cool-idea-nick-5",
+      name: "Idea 5",
+      description:
+        "Lorem 5 ipsum dolor sit amet consectetur adipisicing elit...",
+    },
+  ];
+
+const trpc = initTRPC.create();
+
+export const trpcRouter = trpc.router({
+    getIdeas: trpc.procedure.query(() =>{
+        return { ideas }
+    }),
+});
+
+export type TrpcRouter = typeof trpcRouter;
